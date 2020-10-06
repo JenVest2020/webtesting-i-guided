c56d7ab4d0568ddf6eb9e0535cd8041a489b01e0
JenVest2020 <jenvest2019@gmail.com>
2020-10-05 21:38:53 -0500

    first real test confusion for me

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,6 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+function add(a=0, b=0) {
+  return a + b;
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,10 +1,20 @@
 const { add } = require('./calculator.js');
 
-test('that truth is truth', () => {
-    expect(true).toBe(true);
+// test('that truth is truth', () => {
+//     expect(true).toBe(true);
 
-});
+// });
 
-it('should test that true is true', () => {
-    expect(true).toBe(true);
+// it('should test that true is true', () => {
+//     expect(true).toBe(true);
+// })
+describe('calculator.js', () => {
+
+        describe('add()', () => {
+            it('should return the sum of numbers passed', () => {
+                expect(add (2,2)).toBe(4);
+                expect(add(2,3)).toBe(5);
+            })
+
+        })
 })
